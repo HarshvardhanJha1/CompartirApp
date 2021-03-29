b5b4ecab577fa39419d08951982ea03f7af89742
TheAlchemist1307 <nishitrchouhan@gmail.com>
2021-03-29 21:52:31 +0530

    Added PoolRequests form

--- Components/Pool Tab/PoolRequest.js
+++ Components/Pool Tab/PoolRequest.js
@@ -2,12 +2,23 @@ import React ,{Component} from 'react';
 import { StyleSheet, Text, View,StatusBar,TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Colors from "../constants/colors"
+import SvgUri from "expo-svg-uri";
+import {
+    TextField,
+    FilledTextField,
+    OutlinedTextField,
+  } from 'text-field-rn';
 class PoolRequest extends Component{
     constructor(props)
     {
         super(props)
         this.state={
-            search:false
+            search:false,
+            groupName:'',
+            date:'',
+            time:'',
+            pick:'',
+            drop:""
         }
     }
 
@@ -16,21 +27,79 @@ class PoolRequest extends Component{
         return(
             <View style={styles.container}> 
            <StatusBar  
-                barStyle = "default"   
+                barStyle = "dark"   
                 /> 
-            <Text style={{color:'white'}}>
-            Pool PoolRequest
+                <View style={{height:"10%",flexDirection:'row',alignItems:'center'}}>
+           <TouchableOpacity onPress={()=>{this.props.navigation.pop()}}>
+            <SvgUri width="80" height="80" source={require("../../assets/navbtn.svg")} style={{marginTop:10}}/>
+            </TouchableOpacity>
+            <Text style={{color:Colors.textColor,fontSize:20,fontFamily:'Roboto',alignSelf:'center',textAlign:'center'}}>
+            Create Pool Request
             </Text>
-            <View style={{flexDirection:'row',marginBottom:10,justifyContent:'center'}}>
-                <TouchableOpacity onPress={()=>this.props.navigation.pop()} style={{borderWidth:1,borderRadius:50,borderColor:Colors.textColor}}>
-                    <Text style={{fontFamily:'Roboto' ,color:Colors.textColor,paddingHorizontal:20,paddingVertical:5}}>Chat </Text>
+            </View>
+            <View style={{height:'80%',width:'95%',alignSelf:'center'}}>
+            <OutlinedTextField
+        label='Group Name'
+        textColor={Colors.textColor}
+        value={this.state.groupName}
+         onChangeText={(text)=>{this.setState({groupName:text})}}   
+        ref={this.fieldRef}
+        tintColor={Colors.red}
+        baseColor={Colors.red}	
+        containerStyle={{marginTop:15}}
+      />
+                        <OutlinedTextField
+        label='Date'
+        textColor={Colors.textColor}
+        value={this.state.date}
+         onChangeText={(text)=>{this.setState({date:text})}}   
+        ref={this.fieldRef}
+        tintColor={Colors.red}
+        baseColor={Colors.red}	
+        containerStyle={{marginTop:15}}
+      />
+                  <OutlinedTextField
+        label='Time'
+        textColor={Colors.textColor}
+        value={this.state.time}
+         onChangeText={(text)=>{this.setState({time:text})}}   
+        ref={this.fieldRef}
+        tintColor={Colors.red}
+        baseColor={Colors.red}	
+        containerStyle={{marginTop:15}}
+      />
+                        <OutlinedTextField
+        label='Pick Up'
+        textColor={Colors.textColor}
+        value={this.state.pick}
+         onChangeText={(text)=>{this.setState({pick:text})}}   
+        ref={this.fieldRef}
+        tintColor={Colors.red}
+        baseColor={Colors.red}	
+        containerStyle={{marginTop:15}}
+      />
+            
+            <OutlinedTextField
+        label='Drop Off'
+        textColor={Colors.textColor}
+        value={this.state.drop}
+         onChangeText={(text)=>{this.setState({drop:text})}}   
+        ref={this.fieldRef}
+        tintColor={Colors.red}
+        baseColor={Colors.red}	
+        containerStyle={{marginTop:15}}
+      />
+            
+            </View>
+            <View style={{height:"10%",justifyContent:'center'}}>
+                <TouchableOpacity onPress={()=>{this.props.navigation.pop()}}>
+                <Text style={{backgroundColor:Colors.red,width:'80%',alignSelf:'center',textAlign:'center',paddingVertical:10,borderRadius:20,color:Colors.textColor}}>
+                    Add Request
+                </Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={{borderWidth:1,borderRadius:50,borderColor:Colors.red}}>
-                    <Text style={{fontFamily:'Roboto' ,color:Colors.red,paddingHorizontal:20,paddingVertical:5}}>Cancel </Text>
-                </TouchableOpacity>
+            
+            </View>
 
-
-                </View>
             </View>
         )
     }
